fd0fc947b15f0788e3b2024999c744d5ae3cd954
Henrique Savelli <henrique.savelli@gmail.com>
2021-10-04 16:54:32 -0400

    Refactored Module 1 Code

---
+++ Module-1/Assets/Hamster/Scripts/States/WaitingForDBString.cs
@@ -0,0 +1,17 @@
+﻿namespace Hamster.States
+{
+    // Utility state, for fetching strings.  Is basically
+    // just a specialization of WaitingForDBLoad, but it needs
+    // some minor changes to how the results are parsed, since
+    // they're not technically valid json.
+    class WaitingForDBString : WaitingForDBLoad<string>
+    {
+
+        public WaitingForDBString(string path) : base(path) { }
+
+        protected override string ParseResult(string json)
+        {
+            return json.Length > 2 ? json.Substring(1, json.Length - 2) : null;
+        }
+    }
+}
